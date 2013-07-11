c811c240aa096af32247a9ae2a1a4b584d8d23ce
Kathryn <keb97@cornell.edu>
2013-07-11 13:42:05 -0700

    Add the ability to view a list of all projects

--- Gemfile
+++ Gemfile
@@ -9,11 +9,12 @@ gem 'dynamic_form', '1.1.4'
 gem 'sqlite3'
 
 group :test, :development do
-gem 'rspec-rails', '~> 2.11'
+  gem 'rspec-rails', '~> 2.11'
 end
 
 group :test do
-gem 'capybara', '1.1.2'
+  gem 'capybara', '1.1.2'
+  gem 'factory_girl', '2.6.4'
 end
 
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
 	def index
+        @projects = Project.all
 	end
 	def new
 	  @project = Project.new
 	end
 	def create
-    @project = Project.new(params[:project]) 
+    @project = Project.new(params[:project])
      if @project.save
        flash[:notice] = "Project has been created."
        redirect_to @project
