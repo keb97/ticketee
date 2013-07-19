9202857f2144c52348ac2e7b25167554bec983ae
Kathryn <keb97@cornell.edu>
2013-07-19 13:40:27 -0700

    Restrict ticket updating to only those who have permission

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,40 +2,66 @@ require 'spec_helper'
 
 describe TicketsController do
   let(:user) { Factory(:confirmed_user) }
-let(:project) { Factory(:project) }
-let(:ticket) { Factory(:ticket, :project => project,
-:user => user) }
-context "standard users" do
-it "cannot access a ticket for a project" do
-sign_in(:user, user)
-get :show, :id => ticket.id, :project_id => project.id
-response.should redirect_to(root_path)
-flash[:alert].should eql("The project you were looking " +
-"for could not be found.")
-end
+  let(:project) { Factory(:project) }
+  let(:ticket) { Factory(:ticket, :project => project,
+    :user => user) }
 
-context "with permission to view the project" do
-before do
-sign_in(:user, user)
-define_permission!(user, "view", project)
-end
-def cannot_create_tickets!
-response.should redirect_to(project)
-message = "You cannot create tickets on this project."
-flash[:alert].should eql(message)
-end
-it "cannot begin to create a ticket" do
-get :new, :project_id => project.id
-cannot_create_tickets!
-end
-it "cannot create a ticket without permission" do
-post :create, :project_id => project.id
-cannot_create_tickets!
-end
-end
+  context "standard users" do
+    it "cannot access a ticket for a project" do
+      sign_in(:user, user)
+      get :show, :id => ticket.id, :project_id => project.id
+      response.should redirect_to(root_path)
+      flash[:alert].should eql("The project you were looking " +
+        "for could not be found.")
+    end
+
+  context "with permission to view the project" do
+    before do
+      sign_in(:user, user)
+      define_permission!(user, "view", project)
+    end
+
+    def cannot_create_tickets!
+      response.should redirect_to(project)
+      message = "You cannot create tickets on this project."
+      flash[:alert].should eql(message)
+    end
+
+    def cannot_update_tickets!
+      response.should redirect_to(project)
+      flash[:alert].should eql("You cannot edit tickets on this project.")
+    end
 
 
-end
+    it "cannot begin to create a ticket" do
+      get :new, :project_id => project.id
+      cannot_create_tickets!
+    end
+
+    it "cannot create a ticket without permission" do
+      post :create, :project_id => project.id
+      cannot_create_tickets!
+    end
+
+    it "cannot edit a ticket without permission" do
+      get :edit, { :project_id => project.id, :id => ticket.id }
+      cannot_update_tickets!
+    end
+
+    it "cannot update a ticket without permission" do
+      put :update, { :project_id => project.id,
+        :id => ticket.id,
+        :ticket => {}
+        }
+      cannot_update_tickets!
+    end
+
+
+
+  end
+
+
+  end
 
 
 end
