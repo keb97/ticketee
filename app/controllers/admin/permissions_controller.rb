4b0f6f3edbf177ee2e62872eb11cdde20b4dd2a3
Kathryn <keb97@cornell.edu>
2013-07-20 12:22:55 -0700

    Add permissions screen for admins

--- app/controllers/admin/permissions_controller.rb
+++ app/controllers/admin/permissions_controller.rb
@@ -6,6 +6,21 @@ class Admin::PermissionsController < Admin::BaseController
     @projects = Project.all
   end
 
+  def update
+    @user.permissions.clear
+    params[:permissions].each do |id, permissions|
+      project = Project.find(id)
+      permissions.each do |permission, checked|
+        Permission.create!(:user => @user,
+          :thing => project,
+          :action => permission)
+        end
+      end
+      flash[:notice] = "Permissions updated."
+      redirect_to admin_user_permissions_path
+    end
+
+
   private
     def find_user
       @user = User.find(params[:user_id])
