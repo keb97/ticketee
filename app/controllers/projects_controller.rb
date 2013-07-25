82834f2ff32eb86b58f22c2dbd618be4c01700cd
Kathryn <keb97@cornell.edu>
2013-07-25 16:13:10 -0700

    removed a redundancy in the projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,15 +10,7 @@ class ProjectsController < ApplicationController
 	def new
 	  @project = Project.new
 	end
-	def create
-    @project = Project.new(params[:project])
-     if @project.save
-       flash[:notice] = "Project has been created."
-       redirect_to @project
-     else
-# nothing, yet
-     end
-    end
+
     def show
     end
     def create
